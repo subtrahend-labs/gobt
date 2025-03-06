ce37b7da16b8e39ab9c5b3a5ab444a88b44d82ad
0xcacti <0xcacti@gmail.com>
2025-03-06 14:07:15 -0500

    save

--- main.go
+++ main.go
@@ -3,8 +3,9 @@ package main
 import (
 	"fmt"
 	"log"
-	// "math/big"
+	"math/big"
 	"os"
+	"time"
 
 	gsrpc "github.com/centrifuge/go-substrate-rpc-client/v4"
 	"github.com/centrifuge/go-substrate-rpc-client/v4/signature"
@@ -14,7 +15,8 @@ import (
 )
 
 type AccountInfo struct {
-	Data struct {
+	Nonce types.U32
+	Data  struct {
 		Free     types.U64 // Balance as u64
 		Reserved types.U64
 		Frozen   types.U64 // Now Frozen instead of MiscFrozen
@@ -104,29 +106,94 @@ func main() {
 	fmt.Printf("Frozen balance: %v\n", recipientInfo.Data.Frozen)
 	fmt.Printf("Flags: %v\n", recipientInfo.Data.Flags)
 
-	//bal, ok := new(big.Int).SetString("100000000000000", 10)
-	//if !ok {
-	//	panic(fmt.Errorf("failed to convert balance"))
-	//}
+	bal, ok := new(big.Int).SetString("500000", 10) // Just 0.0005 TAO
+	if !ok {
+		log.Fatalf("Error converting string to big.Int")
+	}
 
-	//
-	//	c, err := types.NewCall(meta, "Balances.transfer", user, types.NewUCompact(bal))
-	//	if err != nil {
-	//		panic(err)
-	//	}
-	//
-	//	ext := types.NewExtrinsic(c)
-	//
-	//	genesisHash, err := api.RPC.Chain.GetBlockHash(0)
-	//	if err != nil {
-	//		panic(err)
-	//	}
+	// Print all modules and their calls
+	// fmt.Println("\nAVAILABLE MODULES AND CALLS:")
+	// for _, module := range meta.AsMetadataV14.Pallets {
+	// 	fmt.Printf("Module: %s (Index: %d)\n", module.Name, module.Index)
 
-	//	rv, err := api.RPC.State.GetRuntimeVersion()
-	//	if err != nil {
-	//		panic(err)
-	//	}
-	//
-	//	senderKey, err := types.CreateStorageKey(meta, "System", "Account", user.AsID[:])
+	// 	// Print calls if they exist
+	// 	if module.HasCalls {
+	// 		// Get the call type ID
+	// 		callTypeID := module.Calls.Type.Int64()
+	// 		fmt.Printf("  Call Type ID: %d\n", callTypeID)
+
+	// 		// Find the type in the lookup
+	// 		if callType, ok := meta.AsMetadataV14.EfficientLookup[callTypeID]; ok {
+	// 			if callType.Def.IsVariant {
+	// 				fmt.Println("  Available Calls:")
+	// 				for _, variant := range callType.Def.Variant.Variants {
+	// 					fmt.Printf("    %s (Index: %d)\n", variant.Name, variant.Index)
+	// 				}
+	// 			}
+	// 		} else {
+	// 			fmt.Printf("  Call type not found in lookup\n")
+	// 		}
+	// 	} else {
+	// 		fmt.Printf("  No calls available\n")
+	// 	}
+	// 	fmt.Println()
+	// }
+
+	c, err := types.NewCall(meta, "Balances.transfer_keep_alive", recipient, types.NewUCompact(bal))
+	if err != nil {
+		log.Fatalf("Error creating call: %s", err)
+	}
+
+	ext := types.NewExtrinsic(c)
+
+	genesisHash, err := api.RPC.Chain.GetBlockHash(0)
+	if err != nil {
+		log.Fatalf("Error getting genesis hash: %s", err)
+	}
+
+	rv, err := api.RPC.State.GetRuntimeVersionLatest()
+	if err != nil {
+		log.Fatalf("Error getting runtime version: %s", err)
+	}
+
+	currentHash, err := api.RPC.Chain.GetBlockHashLatest()
+	if err != nil {
+		log.Fatalf("Error getting current block: %s", err)
+	}
+
+	o := types.SignatureOptions{
+		BlockHash:          currentHash,
+		Era:                types.ExtrinsicEra{IsMortalEra: false},
+		GenesisHash:        genesisHash,
+		Nonce:              types.NewUCompactFromUInt(uint64(uint32(accountInfo.Nonce))),
+		SpecVersion:        rv.SpecVersion,
+		Tip:                types.NewUCompactFromUInt(0),
+		TransactionVersion: rv.TransactionVersion,
+	}
+
+	err = ext.Sign(sender, o)
+	if err != nil {
+		log.Fatalf("Error signing: %s", err)
+	}
+
+	_, err = api.RPC.Author.SubmitExtrinsic(ext)
+	if err != nil {
+		log.Fatalf("Error submitting extrinsic: %s", err)
+	}
+
+	fmt.Println("Extrinsic submitted")
+
+	// Wait for the extrinsic to be included in a block
+	time.Sleep(12 * time.Second)
+
+	// Get the account data
+	ok, err = api.RPC.State.GetStorageLatest(senderKey, &accountInfo)
+	if err != nil {
+		log.Fatalf("Error getting storage: %s", err)
+	}
+	if !ok {
+		log.Fatalf("No storage found")
+	}
+	fmt.Println("Sender balance after transfer: ", accountInfo.Data.Free)
 
 }
