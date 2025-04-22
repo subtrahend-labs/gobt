e933bfe41fb3cd9b1c0ca68b76782d884f0a83af
josh <hey@imjosh.dev>
2025-04-22 12:31:28 -0500

    feat: add stake limit and remove stake

--- extrinsics/subtensor_module.go
+++ extrinsics/subtensor_module.go
@@ -7,7 +7,7 @@ import (
 )
 
 // #### Module: SubtensorModule (Index: 7)
-//     - [ ] set_weights (Index: 0)
+//     - [x] set_weights (Index: 0)
 //     - [ ] batch_set_weights (Index: 80)
 //     - [ ] commit_weights (Index: 96)
 //     - [ ] batch_commit_weights (Index: 100)
@@ -36,7 +36,7 @@ import (
 //     - [ ] sudo (Index: 51)
 //     - [ ] sudo_unchecked_weight (Index: 52)
 //     - [ ] vote (Index: 55)
-//     - [ ] register_network (Index: 59)
+//     - [x] register_network (Index: 59)
 //     - [ ] faucet (Index: 60)
 //     - [ ] dissolve_network (Index: 61)
 //     - [ ] set_children (Index: 67)
@@ -50,10 +50,44 @@ import (
 //     - [ ] move_stake (Index: 85)
 //     - [ ] transfer_stake (Index: 86)
 //     - [ ] swap_stake (Index: 87)
-//     - [ ] add_stake_limit (Index: 88)
-//     - [ ] remove_stake_limit (Index: 89)
+//     - [x] add_stake_limit (Index: 88)
+//     - [x] remove_stake_limit (Index: 89)
 //     - [ ] swap_stake_limit (Index: 90)
 //     - [ ] try_associate_hotkey (Index: 91)
+
+func AddStakeLimitCall(c *client.Client, hotkey types.AccountID, netuid types.U16, amount_staked types.U64, limit_price types.U64, allow_partial types.Bool) (types.Call, error) {
+	call, err := types.NewCall(c.Meta, "SubtensorModule.add_stake_limit", hotkey, netuid, amount_staked, limit_price, allow_partial)
+	if err != nil {
+		return types.Call{}, err
+	}
+	return call, nil
+}
+
+func AddStakeLimitExt(c *client.Client, hotkey types.AccountID, netuid types.U16, amount_staked types.U64, limit_price types.U64, allow_partial types.Bool) (*extrinsic.Extrinsic, error) {
+	call, err := AddStakeLimitCall(c, hotkey, netuid, amount_staked, limit_price, allow_partial)
+	if err != nil {
+		return nil, err
+	}
+	ext := extrinsic.NewExtrinsic(call)
+	return &ext, nil
+}
+
+func RemoveStakeLimitCall(c *client.Client, hotkey types.AccountID, netuid types.U16, amount_unstaked types.U64, limit_price types.U64, allow_partial types.Bool) (types.Call, error) {
+	call, err := types.NewCall(c.Meta, "SubtensorModule.remove_stake_limit", hotkey, netuid, amount_unstaked, limit_price, allow_partial)
+	if err != nil {
+		return types.Call{}, err
+	}
+	return call, nil
+}
+
+func RemoveStakeLimitExt(c *client.Client, hotkey types.AccountID, netuid types.U16, amount_unstaked types.U64, limit_price types.U64, allow_partial types.Bool) (*extrinsic.Extrinsic, error) {
+	call, err := RemoveStakeLimitCall(c, hotkey, netuid, amount_unstaked, limit_price, allow_partial)
+	if err != nil {
+		return nil, err
+	}
+	ext := extrinsic.NewExtrinsic(call)
+	return &ext, nil
+}
 
 func SetWeightsCall(c *client.Client, netuid types.U16, uids []types.U16, weights []types.U16, versionKey types.U64) (types.Call, error) {
 	call, err := types.NewCall(c.Meta, "SubtensorModule.set_weights", netuid, uids, weights, versionKey)
