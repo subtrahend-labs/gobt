91f3d10c7eaddb2744e6bb52ab8ac3b718db266d
0xcacti <0xcacti@gmail.com>
2025-04-23 23:23:10 -0500

    wrap serve_axon

--- extrinsics/subtensor_module_test.go
+++ extrinsics/subtensor_module_test.go
@@ -62,6 +62,64 @@ func TestSubtensorModuleExtrinsics(t *testing.T) {
 		fmt.Println("Here we are again on my own")
 	})
 
+	t.Run("ServeAxon", func(t *testing.T) {
+		t.Parallel()
+		env := setup(t)
+
+		// First, set up a subnet
+		setupSubnet(t, env)
+
+		// Bob needs to register to the subnet first using BurnedRegister
+		netuid := types.NewU16(1)
+		ext, err := BurnedRegisterExt(env.Client, *env.Bob.Hotkey.AccID, netuid)
+		require.NoError(t, err, "Failed to create burned_register ext")
+		testutils.SignAndSubmit(t, env.Client, ext, env.Bob.Coldkey.Keypair, uint32(env.Bob.Coldkey.AccInfo.Nonce))
+		updateUserInfo(t, &env.Bob, env)
+
+		// Now test ServeAxon with Bob's hotkey
+		version := types.NewU32(1)
+
+		// 127.0.0.1 in hex (0x7F000001) converted to big.Int
+		ipInt, _ := new(big.Int).SetString("2130706433", 10) // decimal representation of 127.0.0.1
+		ip := types.NewU128(*ipInt)
+
+		port := types.NewU16(8080)
+		ipType := types.NewU8(4)   // IPv4
+		protocol := types.NewU8(0) // HTTP
+		placeholder1 := types.NewU8(0)
+		placeholder2 := types.NewU8(0)
+
+		// Create an empty certificate for this test
+		certificate := []byte{}
+
+		// Create and submit the ServeAxon extrinsic
+		serveAxonExt, err := ServeAxonExt(
+			env.Client,
+			netuid,
+			version,
+			ip,
+			port,
+			ipType,
+			protocol,
+			placeholder1,
+			placeholder2,
+			certificate,
+		)
+		require.NoError(t, err, "Failed to create serve_axon ext")
+
+		// Sign and submit the extrinsic
+		testutils.SignAndSubmit(
+			t,
+			env.Client,
+			serveAxonExt,
+			env.Bob.Coldkey.Keypair,
+			uint32(env.Bob.Coldkey.AccInfo.Nonce),
+		)
+
+		// Update user info after transaction
+		updateUserInfo(t, &env.Bob, env)
+	})
+
 	// t.Run("SetWeights", func(t *testing.T) {
 	// setup(t)
 	// defer teardown(t)
