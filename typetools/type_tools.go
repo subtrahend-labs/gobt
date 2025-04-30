0f6ecb9bf1faef1b601c0808884d8b781f50da4f
josh <hey@imjosh.dev>
2025-04-30 12:30:53 -0500

    feat: add account to ss58

--- typetools/type_tools.go
+++ typetools/type_tools.go
@@ -1,10 +1,20 @@
 package typetools
 
-import "encoding/binary"
+import (
+	"encoding/binary"
+
+	"github.com/centrifuge/go-substrate-rpc-client/v4/types"
+	"github.com/vedhavyas/go-subkey/v2"
+)
 
 // Needs to be in little endian
 func Uint16ToBytes(n uint16) []byte {
 	b := make([]byte, 2)
 	binary.LittleEndian.PutUint16(b, n)
 	return b
+}
+
+func AccountIDToSS58(acc types.AccountID) string {
+	recipientSS58 := subkey.SS58Encode(acc.ToBytes(), 42)
+	return recipientSS58
 }
