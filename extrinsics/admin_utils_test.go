a5f35e009ebdefd2637c560c58a826f42854a7b0
jkwonn <hellojustinkwon@gmail.com>
2025-06-11 12:31:48 -0500

    feat: sudo difficulty setting tests

--- extrinsics/admin_utils_test.go
+++ extrinsics/admin_utils_test.go
@@ -24,4 +24,64 @@ func TestAdminUtilsModuleExtrinsics(t *testing.T) {
 		testutils.SignAndSubmit(t, env.Client, ext, env.Alice.Coldkey.Keypair, uint32(env.Alice.Coldkey.AccInfo.Nonce))
 		updateUserInfo(t, &env.Alice, env, false)
 	})
+
+	t.Run("SudoSetMaxDifficulty", func(t *testing.T) {
+		t.Parallel()
+		env := setup(t)
+
+		netuid := uint16(0)
+		max_difficulty := types.NewU64(1000)
+		default_difficulty := types.NewU64(0)
+
+		sudoCall, err := SudoSetDifficultyCall(env.Client, netuid, default_difficulty)
+		require.NoError(t, err, "Failed to create sudo_set_difficulty call")
+		sudoCall, err = SudoSetMaxDifficultyCall(env.Client, netuid, max_difficulty)
+		require.NoError(t, err, "Failed to create sudo_set_min_difficulty call")
+
+		ext, err := NewSudoExt(env.Client, &sudoCall)
+		require.NoError(t, err, "Failed to create sudo extrinsic")
+
+		testutils.SignAndSubmit(t, env.Client, ext, env.Alice.Coldkey.Keypair, uint32(env.Alice.Coldkey.AccInfo.Nonce))
+		updateUserInfo(t, &env.Alice, env, false)
+	})
+
+	t.Run("SudoSetDifficulty", func(t *testing.T) {
+		t.Parallel()
+		env := setup(t)
+
+		netuid := uint16(0)
+		default_difficulty := types.NewU64(0)
+
+		sudoCall, err := SudoSetDifficultyCall(env.Client, netuid, default_difficulty)
+		require.NoError(t, err, "Failed to create sudo_set_difficulty call")
+
+		// Wrap in sudo extrinsic
+		ext, err := NewSudoExt(env.Client, &sudoCall)
+		require.NoError(t, err, "Failed to create sudo extrinsic")
+
+		// Submit as root (usually Alice in testnets)
+		testutils.SignAndSubmit(t, env.Client, ext, env.Alice.Coldkey.Keypair, uint32(env.Alice.Coldkey.AccInfo.Nonce))
+		updateUserInfo(t, &env.Alice, env, false)
+	})
+
+	t.Run("SudoSetMinDifficulty", func(t *testing.T) {
+		t.Parallel()
+		env := setup(t)
+
+		netuid := uint16(0)
+		default_difficulty := types.NewU64(0)
+		min_difficulty := types.NewU64(100)
+
+		sudoCall, err := SudoSetDifficultyCall(env.Client, netuid, default_difficulty)
+		require.NoError(t, err, "Failed to create sudo_set_difficulty call")
+		sudoCall, err = SudoSetMinDifficultyCall(env.Client, netuid, min_difficulty)
+		require.NoError(t, err, "Failed to create sudo_set_min_difficulty call")
+
+		ext, err := NewSudoExt(env.Client, &sudoCall)
+		require.NoError(t, err, "Failed to create sudo extrinsic")
+
+		testutils.SignAndSubmit(t, env.Client, ext, env.Alice.Coldkey.Keypair, uint32(env.Alice.Coldkey.AccInfo.Nonce))
+		updateUserInfo(t, &env.Alice, env, false)
+	})
+
 }
--- go.mod
+++ go.mod
@@ -1,6 +1,8 @@
 module github.com/subtrahend-labs/gobt
 
-go 1.23.6
+go 1.23.0
+
+toolchain go1.24.1
 
 require (
 	github.com/ChainSafe/go-schnorrkel v1.1.0
