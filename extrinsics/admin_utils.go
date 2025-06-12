ede85d057f63b20db029bf4680649658903a0447
Ryandpark2006 <ryandpark2006@gmail.com>
2025-06-12 09:36:49 -0500

    updated test cases and implementation for 4 adminutils

--- extrinsics/admin_utils.go
+++ extrinsics/admin_utils.go
@@ -4,6 +4,7 @@ import (
 	"github.com/centrifuge/go-substrate-rpc-client/v4/types"
 	"github.com/centrifuge/go-substrate-rpc-client/v4/types/extrinsic"
 	"github.com/subtrahend-labs/gobt/client"
+	"github.com/subtrahend-labs/gobt/runtime"
 )
 
 func SudoSetNetworkRateLimitCall(c *client.Client, rateLimit types.U64) (types.Call, error) {
@@ -20,6 +21,94 @@ func SudoSetNetworkRateLimitCall(c *client.Client, rateLimit types.U64) (types.C
 
 func SudoSetNetworkRateLimitExt(c *client.Client, rateLimit types.U64) (*extrinsic.Extrinsic, error) {
 	call, err := SudoSetNetworkRateLimitCall(c, rateLimit)
+	if err != nil {
+		return nil, err
+	}
+	ext := extrinsic.NewExtrinsic(call)
+	return &ext, nil
+}
+
+// - [ ] sudo_toggle_evm_precompile (Index: 62)
+func SudoToggleEvmPrecompileCall(c *client.Client, precompileId types.U8, enabled types.Bool) (types.Call, error) {
+	call, err := types.NewCall(
+		c.Meta,
+		"AdminUtils.sudo_toggle_evm_precompile",
+		precompileId,
+		enabled,
+	)
+	if err != nil {
+		return types.Call{}, err
+	}
+	return call, nil
+}
+
+func SudoToggleEvmPrecompileExt(c *client.Client, precompileId types.U8, enabled types.Bool) (*extrinsic.Extrinsic, error) {
+	call, err := SudoToggleEvmPrecompileCall(c, precompileId, enabled)
+	if err != nil {
+		return nil, err
+	}
+	ext := extrinsic.NewExtrinsic(call)
+	return &ext, nil
+}
+
+func SudoSetSubnetMovingAlphaCall(c *client.Client, alpha runtime.I96F32) (types.Call, error) {
+	call, err := types.NewCall(
+		c.Meta,
+		"AdminUtils.sudo_set_subnet_moving_alpha",
+		alpha,
+	)
+	if err != nil {
+		return types.Call{}, err
+	}
+	return call, nil
+}
+
+func SudoSetSubnetMovingAlphaExt(c *client.Client, alpha runtime.I96F32) (*extrinsic.Extrinsic, error) {
+	call, err := SudoSetSubnetMovingAlphaCall(c, alpha)
+	if err != nil {
+		return nil, err
+	}
+	ext := extrinsic.NewExtrinsic(call)
+	return &ext, nil
+}
+
+func SudoSetSubnetOwnerHotkeyCall(c *client.Client, netuid types.U16, hotkey types.AccountID) (types.Call, error) {
+	call, err := types.NewCall(
+		c.Meta,
+		"AdminUtils.sudo_set_subnet_owner_hotkey",
+		netuid,
+		hotkey,
+	)
+	if err != nil {
+		return types.Call{}, err
+	}
+	return call, nil
+}
+
+func SudoSetSubnetOwnerHotkeyExt(c *client.Client, netuid types.U16, hotkey types.AccountID) (*extrinsic.Extrinsic, error) {
+	call, err := SudoSetSubnetOwnerHotkeyCall(c, netuid, hotkey)
+	if err != nil {
+		return nil, err
+	}
+	ext := extrinsic.NewExtrinsic(call)
+	return &ext, nil
+}
+
+func SudoSetEmaPriceHalvingPeriodCall(c *client.Client, netuid types.U16, emaHalving types.U64) (types.Call, error) {
+	call, err := types.NewCall(
+		c.Meta,
+		"AdminUtils.sudo_set_ema_price_halving_period",
+		netuid,
+		emaHalving,
+	)
+	if err != nil {
+		return types.Call{}, err
+	}
+	return call, nil
+}
+
+func SudoSetEmaPriceHalvingPeriodExt(c *client.Client, netuid types.U16, emaHalving types.U64) (*extrinsic.Extrinsic, error) {
+	call, err := SudoSetEmaPriceHalvingPeriodCall(c, netuid, emaHalving)
 	if err != nil {
 		return nil, err
 	}
--- extrinsics/admin_utils_test.go
+++ extrinsics/admin_utils_test.go
@@ -4,10 +4,12 @@
 package extrinsics
 
 import (
+	"math/big"
 	"testing"
 
 	"github.com/centrifuge/go-substrate-rpc-client/v4/types"
 	"github.com/stretchr/testify/require"
+	"github.com/subtrahend-labs/gobt/runtime"
 	"github.com/subtrahend-labs/gobt/testutils"
 )
 
@@ -22,7 +24,79 @@ func TestAdminUtilsModuleExtrinsics(t *testing.T) {
 		require.NoError(t, err, "Failed to create sudo_set_network_rate_limit ext")
 		ext, _ := NewSudoExt(env.Client, &sudoCall)
 		testutils.SignAndSubmit(t, env.Client, ext, env.Alice.Coldkey.Keypair, uint32(env.Alice.Coldkey.AccInfo.Nonce))
-		updateUserInfo(t, &env.Alice, env, false)
+	})
+
+	t.Run("SudoToggleEvmPrecompile", func(t *testing.T) {
+		t.Parallel()
+		env := setup(t)
+
+		call, err := SudoToggleEvmPrecompileCall(env.Client, types.U8(1), types.Bool(true))
+		require.NoError(t, err, "Failed to create sudo_toggle_evm_precompile call")
+
+		sudoExt, err := NewSudoExt(env.Client, &call)
+		require.NoError(t, err, "Failed to create sudo ext")
+
+		testutils.SignAndSubmit(t, env.Client, sudoExt, env.Alice.Coldkey.Keypair, uint32(env.Alice.Coldkey.AccInfo.Nonce))
+	})
+
+	t.Run("SudoSetSubnetMovingAlpha", func(t *testing.T) {
+		t.Parallel()
+		env := setup(t)
+
+		alpha := runtime.I96F32{
+			Bits: types.NewU128(*big.NewInt(1000000000)),
+		}
+
+		ext, err := SudoSetSubnetMovingAlphaExt(env.Client, alpha)
+		require.NoError(t, err)
+
+		sudoExt, err := NewSudoExt(env.Client, &ext.Method)
+		require.NoError(t, err)
+
+		blockHash := testutils.SignAndSubmit(t, env.Client, sudoExt, env.Alice.Coldkey.Keypair, uint32(env.Alice.Coldkey.AccInfo.Nonce))
+		require.NotNil(t, blockHash)
+
+		meta, _ := env.Client.Api.RPC.State.GetMetadataLatest()
+		storageKey, _ := types.CreateStorageKey(meta, "SubtensorModule", "SubnetMovingAlpha")
+		var result runtime.I96F32
+		env.Client.Api.RPC.State.GetStorageLatest(storageKey, &result)
+
+		require.Equal(t, alpha.Bits, result.Bits)
+		t.Logf("SubnetMovingAlpha updated successfully: %v", result.Bits)
+	})
+
+	t.Run("SudoSetSubnetOwnerHotkey", func(t *testing.T) {
+		t.Parallel()
+		env := setup(t)
+		setupSubnet(t, env)
+
+		ext, err := SudoSetSubnetOwnerHotkeyExt(env.Client, types.U16(1), *env.Bob.Hotkey.AccID)
+		require.NoError(t, err)
+
+		sudoExt, err := NewSudoExt(env.Client, &ext.Method)
+		require.NoError(t, err)
+
+		blockHash := testutils.SignAndSubmit(t, env.Client, sudoExt, env.Alice.Coldkey.Keypair, uint32(env.Alice.Coldkey.AccInfo.Nonce))
+		require.NotNil(t, blockHash, "Transaction should be included in block")
+
+		t.Logf("SudoSetSubnetOwnerHotkey transaction included in block: %x", blockHash)
+	})
+
+	t.Run("SudoSetEmaPriceHalvingPeriod", func(t *testing.T) {
+		t.Parallel()
+		env := setup(t)
+		setupSubnet(t, env)
+
+		ext, err := SudoSetEmaPriceHalvingPeriodExt(env.Client, types.U16(1), types.U64(7200))
+		require.NoError(t, err, "Failed to create sudo_set_ema_price_halving_period ext")
+
+		sudoExt, err := NewSudoExt(env.Client, &ext.Method)
+		require.NoError(t, err, "Failed to create sudo ext")
+
+		blockHash := testutils.SignAndSubmit(t, env.Client, sudoExt, env.Alice.Coldkey.Keypair, uint32(env.Alice.Coldkey.AccInfo.Nonce))
+		require.NotNil(t, blockHash, "Transaction should be included in block")
+
+		t.Logf("SudoSetEmaPriceHalvingPeriod transaction included in block: %x", blockHash)
 	})
 
 }
