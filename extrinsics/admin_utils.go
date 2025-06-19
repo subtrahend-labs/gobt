8a3100e073d5bdfeebed2db288803ce9962cdbd4
stationarybird <william.z.gu@gmail.com>
2025-06-19 12:05:31 -0500

    feat: more admin utils methods

--- extrinsics/admin_utils.go
+++ extrinsics/admin_utils.go
@@ -90,3 +90,442 @@ func SudoSetServingRateLimitExt(c *client.Client, netuid types.U16, serving_rate
 	ext := extrinsic.NewExtrinsic(call)
 	return &ext, nil
 }
+
+func SudoSetAdjustmentIntervalCall(c *client.Client, netuid types.U16, adjustment_interval types.U16) (types.Call, error) {
+	call, err := types.NewCall(
+		c.Meta,
+		"AdminUtils.sudo_set_adjustment_interval",
+		netuid,
+		adjustment_interval,
+	)
+	if err != nil {
+		return types.Call{}, err
+	}
+	return call, nil
+}
+
+func SudoSetAdjustmentIntervalExt(c *client.Client, netuid types.U16, adjustment_interval types.U16) (*extrinsic.Extrinsic, error) {
+	call, err := SudoSetAdjustmentIntervalCall(c, netuid, adjustment_interval)
+	if err != nil {
+		return nil, err
+	}
+	ext := extrinsic.NewExtrinsic(call)
+	return &ext, nil
+}
+
+func SudoSetAdjustmentAlphaCall(c *client.Client, netuid types.U16, adjustment_alpha types.U64) (types.Call, error) {
+	call, err := types.NewCall(
+		c.Meta,
+		"AdminUtils.sudo_set_adjustment_alpha",
+		netuid,
+		adjustment_alpha,
+	)
+	if err != nil {
+		return types.Call{}, err
+	}
+	return call, nil
+}
+
+func SudoSetAdjustmentAlphaExt(c *client.Client, netuid types.U16, adjustment_alpha types.U64) (*extrinsic.Extrinsic, error) {
+	call, err := SudoSetAdjustmentAlphaCall(c, netuid, adjustment_alpha)
+	if err != nil {
+		return nil, err
+	}
+	ext := extrinsic.NewExtrinsic(call)
+	return &ext, nil
+}
+
+func SudoSetMaxWeightLimitCall(c *client.Client, netuid types.U16, max_weight_limit types.U16) (types.Call, error) {
+	call, err := types.NewCall(
+		c.Meta,
+		"AdminUtils.sudo_set_max_weight_limit",
+		netuid,
+		max_weight_limit,
+	)
+	if err != nil {
+		return types.Call{}, err
+	}
+	return call, nil
+}
+
+func SudoSetMaxWeightLimitExt(c *client.Client, netuid types.U16, max_weight_limit types.U16) (*extrinsic.Extrinsic, error) {
+	call, err := SudoSetMaxWeightLimitCall(c, netuid, max_weight_limit)
+	if err != nil {
+		return nil, err
+	}
+	ext := extrinsic.NewExtrinsic(call)
+	return &ext, nil
+}
+
+func SudoSetImmunityPeriodCall(c *client.Client, netuid types.U16, immunity_period types.U16) (types.Call, error) {
+	call, err := types.NewCall(
+		c.Meta,
+		"AdminUtils.sudo_set_immunity_period",
+		netuid,
+		immunity_period,
+	)
+	if err != nil {
+		return types.Call{}, err
+	}
+	return call, nil
+}
+
+func SudoSetImmunityPeriodExt(c *client.Client, netuid types.U16, immunity_period types.U16) (*extrinsic.Extrinsic, error) {
+	call, err := SudoSetImmunityPeriodCall(c, netuid, immunity_period)
+	if err != nil {
+		return nil, err
+	}
+	ext := extrinsic.NewExtrinsic(call)
+	return &ext, nil
+}
+
+func SudoSetMinAllowedWeightsCall(c *client.Client, netuid types.U16, min_allowed_weights types.U16) (types.Call, error) {
+	call, err := types.NewCall(
+		c.Meta,
+		"AdminUtils.sudo_set_min_allowed_weights",
+		netuid,
+		min_allowed_weights,
+	)
+	if err != nil {
+		return types.Call{}, err
+	}
+	return call, nil
+}
+
+func SudoSetMinAllowedWeightsExt(c *client.Client, netuid types.U16, min_allowed_weights types.U16) (*extrinsic.Extrinsic, error) {
+	call, err := SudoSetMinAllowedWeightsCall(c, netuid, min_allowed_weights)
+	if err != nil {
+		return nil, err
+	}
+	ext := extrinsic.NewExtrinsic(call)
+	return &ext, nil
+}
+
+func SudoSetMaxAllowedUidsCall(c *client.Client, netuid types.U16, max_allowed_uids types.U16) (types.Call, error) {
+	call, err := types.NewCall(
+		c.Meta,
+		"AdminUtils.sudo_set_max_allowed_uids",
+		netuid,
+		max_allowed_uids,
+	)
+	if err != nil {
+		return types.Call{}, err
+	}
+	return call, nil
+}
+
+func SudoSetMaxAllowedUidsExt(c *client.Client, netuid types.U16, max_allowed_uids types.U16) (*extrinsic.Extrinsic, error) {
+	call, err := SudoSetMaxAllowedUidsCall(c, netuid, max_allowed_uids)
+	if err != nil {
+		return nil, err
+	}
+	ext := extrinsic.NewExtrinsic(call)
+	return &ext, nil
+}
+
+func SudoSetKappaCall(c *client.Client, netuid types.U16, kappa types.U16) (types.Call, error) {
+	call, err := types.NewCall(
+		c.Meta,
+		"AdminUtils.sudo_set_kappa",
+		netuid,
+		kappa,
+	)
+	if err != nil {
+		return types.Call{}, err
+	}
+	return call, nil
+}
+
+func SudoSetKappaExt(c *client.Client, netuid types.U16, kappa types.U16) (*extrinsic.Extrinsic, error) {
+	call, err := SudoSetKappaCall(c, netuid, kappa)
+	if err != nil {
+		return nil, err
+	}
+	ext := extrinsic.NewExtrinsic(call)
+	return &ext, nil
+}
+
+func SudoSetRhoCall(c *client.Client, netuid types.U16, rho types.U16) (types.Call, error) {
+	call, err := types.NewCall(
+		c.Meta,
+		"AdminUtils.sudo_set_rho",
+		netuid,
+		rho,
+	)
+	if err != nil {
+		return types.Call{}, err
+	}
+	return call, nil
+}
+
+func SudoSetRhoExt(c *client.Client, netuid types.U16, rho types.U16) (*extrinsic.Extrinsic, error) {
+	call, err := SudoSetRhoCall(c, netuid, rho)
+	if err != nil {
+		return nil, err
+	}
+	ext := extrinsic.NewExtrinsic(call)
+	return &ext, nil
+}
+
+func SudoSetActivityCutoffCall(c *client.Client, netuid types.U16, activity_cutoff types.U16) (types.Call, error) {
+	call, err := types.NewCall(
+		c.Meta,
+		"AdminUtils.sudo_set_activity_cutoff",
+		netuid,
+		activity_cutoff,
+	)
+	if err != nil {
+		return types.Call{}, err
+	}
+	return call, nil
+}
+
+func SudoSetActivityCutoffExt(c *client.Client, netuid types.U16, activity_cutoff types.U16) (*extrinsic.Extrinsic, error) {
+	call, err := SudoSetActivityCutoffCall(c, netuid, activity_cutoff)
+	if err != nil {
+		return nil, err
+	}
+	ext := extrinsic.NewExtrinsic(call)
+	return &ext, nil
+}
+
+func SudoSetNetworkRegistrationAllowedCall(c *client.Client, netuid types.U16, registration_allowed bool) (types.Call, error) {
+	call, err := types.NewCall(
+		c.Meta,
+		"AdminUtils.sudo_set_network_registration_allowed",
+		netuid,
+		registration_allowed,
+	)
+	if err != nil {
+		return types.Call{}, err
+	}
+	return call, nil
+}
+
+func SudoSetNetworkRegistrationAllowedExt(c *client.Client, netuid types.U16, registration_allowed bool) (*extrinsic.Extrinsic, error) {
+	call, err := SudoSetNetworkRegistrationAllowedCall(c, netuid, registration_allowed)
+	if err != nil {
+		return nil, err
+	}
+	ext := extrinsic.NewExtrinsic(call)
+	return &ext, nil
+}
+
+func SudoSetNetworkPowRegistrationAllowedCall(c *client.Client, netuid types.U16, registration_allowed bool) (types.Call, error) {
+	call, err := types.NewCall(
+		c.Meta,
+		"AdminUtils.sudo_set_network_pow_registration_allowed",
+		netuid,
+		registration_allowed,
+	)
+	if err != nil {
+		return types.Call{}, err
+	}
+	return call, nil
+}
+
+func SudoSetNetworkPowRegistrationAllowedExt(c *client.Client, netuid types.U16, registration_allowed bool) (*extrinsic.Extrinsic, error) {
+	call, err := SudoSetNetworkPowRegistrationAllowedCall(c, netuid, registration_allowed)
+	if err != nil {
+		return nil, err
+	}
+	ext := extrinsic.NewExtrinsic(call)
+	return &ext, nil
+}
+
+func SudoSetTargetRegistrationsPerIntervalCall(c *client.Client, netuid types.U16, target_registrations_per_interval types.U16) (types.Call, error) {
+	call, err := types.NewCall(
+		c.Meta,
+		"AdminUtils.sudo_set_target_registrations_per_interval",
+		netuid,
+		target_registrations_per_interval,
+	)
+	if err != nil {
+		return types.Call{}, err
+	}
+	return call, nil
+}
+
+func SudoSetTargetRegistrationsPerIntervalExt(c *client.Client, netuid types.U16, target_registrations_per_interval types.U16) (*extrinsic.Extrinsic, error) {
+	call, err := SudoSetTargetRegistrationsPerIntervalCall(c, netuid, target_registrations_per_interval)
+	if err != nil {
+		return nil, err
+	}
+	ext := extrinsic.NewExtrinsic(call)
+	return &ext, nil
+}
+
+func SudoSetMinBurnCall(c *client.Client, netuid types.U16, min_burn types.U64) (types.Call, error) {
+	call, err := types.NewCall(
+		c.Meta,
+		"AdminUtils.sudo_set_min_burn",
+		netuid,
+		min_burn,
+	)
+	if err != nil {
+		return types.Call{}, err
+	}
+	return call, nil
+}
+
+func SudoSetMinBurnExt(c *client.Client, netuid types.U16, min_burn types.U64) (*extrinsic.Extrinsic, error) {
+	call, err := SudoSetMinBurnCall(c, netuid, min_burn)
+	if err != nil {
+		return nil, err
+	}
+	ext := extrinsic.NewExtrinsic(call)
+	return &ext, nil
+}
+
+func SudoSetMaxBurnCall(c *client.Client, netuid types.U16, max_burn types.U64) (types.Call, error) {
+	call, err := types.NewCall(
+		c.Meta,
+		"AdminUtils.sudo_set_max_burn",
+		netuid,
+		max_burn,
+	)
+	if err != nil {
+		return types.Call{}, err
+	}
+	return call, nil
+}
+
+func SudoSetMaxBurnExt(c *client.Client, netuid types.U16, max_burn types.U64) (*extrinsic.Extrinsic, error) {
+	call, err := SudoSetMaxBurnCall(c, netuid, max_burn)
+	if err != nil {
+		return nil, err
+	}
+	ext := extrinsic.NewExtrinsic(call)
+	return &ext, nil
+}
+
+func SudoSetDifficultyCall(c *client.Client, netuid types.U16, difficulty types.U64) (types.Call, error) {
+	call, err := types.NewCall(
+		c.Meta,
+		"AdminUtils.sudo_set_difficulty",
+		netuid,
+		difficulty,
+	)
+	if err != nil {
+		return types.Call{}, err
+	}
+	return call, nil
+}
+
+func SudoSetDifficultyExt(c *client.Client, netuid types.U16, difficulty types.U64) (*extrinsic.Extrinsic, error) {
+	call, err := SudoSetDifficultyCall(c, netuid, difficulty)
+	if err != nil {
+		return nil, err
+	}
+	ext := extrinsic.NewExtrinsic(call)
+	return &ext, nil
+}
+
+func SudoSetMaxAllowedValidatorsCall(c *client.Client, netuid types.U16, max_allowed_validators types.U16) (types.Call, error) {
+	call, err := types.NewCall(
+		c.Meta,
+		"AdminUtils.sudo_set_max_allowed_validators",
+		netuid,
+		max_allowed_validators,
+	)
+	if err != nil {
+		return types.Call{}, err
+	}
+	return call, nil
+}
+
+func SudoSetMaxAllowedValidatorsExt(c *client.Client, netuid types.U16, max_allowed_validators types.U16) (*extrinsic.Extrinsic, error) {
+	call, err := SudoSetMaxAllowedValidatorsCall(c, netuid, max_allowed_validators)
+	if err != nil {
+		return nil, err
+	}
+	ext := extrinsic.NewExtrinsic(call)
+	return &ext, nil
+}
+
+func SudoSetBondsMovingAverageCall(c *client.Client, netuid types.U16, bonds_moving_average types.U64) (types.Call, error) {
+	call, err := types.NewCall(
+		c.Meta,
+		"AdminUtils.sudo_set_bonds_moving_average",
+		netuid,
+		bonds_moving_average,
+	)
+	if err != nil {
+		return types.Call{}, err
+	}
+	return call, nil
+}
+
+func SudoSetBondsMovingAverageExt(c *client.Client, netuid types.U16, bonds_moving_average types.U64) (*extrinsic.Extrinsic, error) {
+	call, err := SudoSetBondsMovingAverageCall(c, netuid, bonds_moving_average)
+	if err != nil {
+		return nil, err
+	}
+	ext := extrinsic.NewExtrinsic(call)
+	return &ext, nil
+}
+
+func SudoSetBondsPenaltyCall(c *client.Client, netuid types.U16, bonds_penalty types.U16) (types.Call, error) {
+	call, err := types.NewCall(
+		c.Meta,
+		"AdminUtils.sudo_set_bonds_penalty",
+		netuid,
+		bonds_penalty,
+	)
+	if err != nil {
+		return types.Call{}, err
+	}
+	return call, nil
+}
+
+func SudoSetBondsPenaltyExt(c *client.Client, netuid types.U16, bonds_penalty types.U16) (*extrinsic.Extrinsic, error) {
+	call, err := SudoSetBondsPenaltyCall(c, netuid, bonds_penalty)
+	if err != nil {
+		return nil, err
+	}
+	ext := extrinsic.NewExtrinsic(call)
+	return &ext, nil
+}
+
+func SudoSetMaxRegistrationsPerBlockCall(c *client.Client, netuid types.U16, max_registrations_per_block types.U16) (types.Call, error) {
+	call, err := types.NewCall(
+		c.Meta,
+		"AdminUtils.sudo_set_max_registrations_per_block",
+		netuid,
+		max_registrations_per_block,
+	)
+	if err != nil {
+		return types.Call{}, err
+	}
+	return call, nil
+}
+
+func SudoSetMaxRegistrationsPerBlockExt(c *client.Client, netuid types.U16, max_registrations_per_block types.U16) (*extrinsic.Extrinsic, error) {
+	call, err := SudoSetMaxRegistrationsPerBlockCall(c, netuid, max_registrations_per_block)
+	if err != nil {
+		return nil, err
+	}
+	ext := extrinsic.NewExtrinsic(call)
+	return &ext, nil
+}
+
+func SudoSetSubnetOwnerCutCall(c *client.Client, subnet_owner_cut types.U16) (types.Call, error) {
+	call, err := types.NewCall(
+		c.Meta,
+		"AdminUtils.sudo_set_subnet_owner_cut",
+		subnet_owner_cut,
+	)
+	if err != nil {
+		return types.Call{}, err
+	}
+	return call, nil
+}
+
+func SudoSetSubnetOwnerCutExt(c *client.Client, subnet_owner_cut types.U16) (*extrinsic.Extrinsic, error) {
+	call, err := SudoSetSubnetOwnerCutCall(c, subnet_owner_cut)
+	if err != nil {
+		return nil, err
+	}
+	ext := extrinsic.NewExtrinsic(call)
+	return &ext, nil
+}
--- extrinsics/admin_utils_test.go
+++ extrinsics/admin_utils_test.go
@@ -75,4 +75,332 @@ func TestAdminUtilsModuleExtrinsics(t *testing.T) {
 		require.Equal(t, servingRateLimit, newServingRateLimit, "Serving rate limit was not updated correctly")
 	})
 
+	t.Run("SudoSetAdjustmentInterval", func(t *testing.T) {
+		t.Parallel()
+		env := setup(t)
+
+		adjustmentInterval := types.NewU16(1000)
+		sudoCall, _ := SudoSetAdjustmentIntervalCall(env.Client, 0, adjustmentInterval)
+		ext, _ := NewSudoExt(env.Client, &sudoCall)
+		testutils.SignAndSubmit(t, env.Client, ext, env.Alice.Coldkey.Keypair, uint32(env.Alice.Coldkey.AccInfo.Nonce))
+
+		storageKey, _ := types.CreateStorageKey(env.Client.Meta, "SubtensorModule", "AdjustmentInterval", typetools.Uint16ToBytes(uint16(0)))
+		var newAdjustmentInterval types.U16
+		env.Client.Api.RPC.State.GetStorageLatest(storageKey, &newAdjustmentInterval)
+
+		require.Equal(t, adjustmentInterval, newAdjustmentInterval, "Adjustment interval was not updated correctly")
+	})
+
+	t.Run("SudoSetAdjustmentAlpha", func(t *testing.T) {
+		t.Parallel()
+		env := setup(t)
+
+		adjustmentAlpha := types.NewU64(1000)
+		sudoCall, _ := SudoSetAdjustmentAlphaCall(env.Client, 0, adjustmentAlpha)
+		ext, _ := NewSudoExt(env.Client, &sudoCall)
+		testutils.SignAndSubmit(t, env.Client, ext, env.Alice.Coldkey.Keypair, uint32(env.Alice.Coldkey.AccInfo.Nonce))
+
+		storageKey, _ := types.CreateStorageKey(env.Client.Meta, "SubtensorModule", "AdjustmentAlpha", typetools.Uint16ToBytes(uint16(0)))
+		var newAdjustmentAlpha types.U64
+		env.Client.Api.RPC.State.GetStorageLatest(storageKey, &newAdjustmentAlpha)
+
+		require.Equal(t, adjustmentAlpha, newAdjustmentAlpha, "Adjustment alpha was not updated correctly")
+	})
+
+	t.Run("SudoSetMaxWeightLimit", func(t *testing.T) {
+		t.Parallel()
+		env := setup(t)
+
+		maxWeightLimit := types.NewU16(1000)
+		sudoCall, _ := SudoSetMaxWeightLimitCall(env.Client, 0, maxWeightLimit)
+		ext, _ := NewSudoExt(env.Client, &sudoCall)
+		testutils.SignAndSubmit(t, env.Client, ext, env.Alice.Coldkey.Keypair, uint32(env.Alice.Coldkey.AccInfo.Nonce))
+
+		storageKey, _ := types.CreateStorageKey(env.Client.Meta, "SubtensorModule", "MaxWeightsLimit", typetools.Uint16ToBytes(uint16(0)))
+		var newMaxWeightLimit types.U16
+		env.Client.Api.RPC.State.GetStorageLatest(storageKey, &newMaxWeightLimit)
+
+		require.Equal(t, maxWeightLimit, newMaxWeightLimit, "Max weight limit was not updated correctly")
+	})
+
+	t.Run("SudoSetImmunityPeriod", func(t *testing.T) {
+		t.Parallel()
+		env := setup(t)
+
+		immunityPeriod := types.NewU16(1000)
+		sudoCall, _ := SudoSetImmunityPeriodCall(env.Client, 0, immunityPeriod)
+		ext, _ := NewSudoExt(env.Client, &sudoCall)
+		testutils.SignAndSubmit(t, env.Client, ext, env.Alice.Coldkey.Keypair, uint32(env.Alice.Coldkey.AccInfo.Nonce))
+
+		storageKey, _ := types.CreateStorageKey(env.Client.Meta, "SubtensorModule", "ImmunityPeriod", typetools.Uint16ToBytes(uint16(0)))
+		var newImmunityPeriod types.U16
+		env.Client.Api.RPC.State.GetStorageLatest(storageKey, &newImmunityPeriod)
+
+		require.Equal(t, immunityPeriod, newImmunityPeriod, "Immunity period was not updated correctly")
+	})
+
+	t.Run("SudoSetMinAllowedWeights", func(t *testing.T) {
+		t.Parallel()
+		env := setup(t)
+
+		minAllowedWeights := types.NewU16(1000)
+		sudoCall, _ := SudoSetMinAllowedWeightsCall(env.Client, 0, minAllowedWeights)
+		ext, _ := NewSudoExt(env.Client, &sudoCall)
+		testutils.SignAndSubmit(t, env.Client, ext, env.Alice.Coldkey.Keypair, uint32(env.Alice.Coldkey.AccInfo.Nonce))
+
+		storageKey, _ := types.CreateStorageKey(env.Client.Meta, "SubtensorModule", "MinAllowedWeights", typetools.Uint16ToBytes(uint16(0)))
+		var newMinAllowedWeights types.U16
+		env.Client.Api.RPC.State.GetStorageLatest(storageKey, &newMinAllowedWeights)
+
+		require.Equal(t, minAllowedWeights, newMinAllowedWeights, "Min allowed weights was not updated correctly")
+	})
+	t.Run("SudoSetMaxAllowedUids", func(t *testing.T) {
+		t.Parallel()
+		env := setup(t)
+
+		maxAllowedUids := types.NewU16(1000)
+		sudoCall, _ := SudoSetMaxAllowedUidsCall(env.Client, 0, maxAllowedUids)
+		ext, _ := NewSudoExt(env.Client, &sudoCall)
+		testutils.SignAndSubmit(t, env.Client, ext, env.Alice.Coldkey.Keypair, uint32(env.Alice.Coldkey.AccInfo.Nonce))
+
+		storageKey, _ := types.CreateStorageKey(env.Client.Meta, "SubtensorModule", "MaxAllowedUids", typetools.Uint16ToBytes(uint16(0)))
+		var newMaxAllowedUids types.U16
+		env.Client.Api.RPC.State.GetStorageLatest(storageKey, &newMaxAllowedUids)
+
+		require.Equal(t, maxAllowedUids, newMaxAllowedUids, "Max allowed uids was not updated correctly")
+	})
+
+	t.Run("SudoSetKappa", func(t *testing.T) {
+		t.Parallel()
+		env := setup(t)
+
+		kappa := types.NewU16(1000)
+		sudoCall, _ := SudoSetKappaCall(env.Client, 0, kappa)
+		ext, _ := NewSudoExt(env.Client, &sudoCall)
+		testutils.SignAndSubmit(t, env.Client, ext, env.Alice.Coldkey.Keypair, uint32(env.Alice.Coldkey.AccInfo.Nonce))
+
+		storageKey, _ := types.CreateStorageKey(env.Client.Meta, "SubtensorModule", "Kappa", typetools.Uint16ToBytes(uint16(0)))
+		var newKappa types.U16
+		env.Client.Api.RPC.State.GetStorageLatest(storageKey, &newKappa)
+
+		require.Equal(t, kappa, newKappa, "Kappa was not updated correctly")
+	})
+
+	t.Run("SudoSetRho", func(t *testing.T) {
+		t.Parallel()
+		env := setup(t)
+
+		rho := types.NewU16(1000)
+		sudoCall, _ := SudoSetRhoCall(env.Client, 0, rho)
+		ext, _ := NewSudoExt(env.Client, &sudoCall)
+		testutils.SignAndSubmit(t, env.Client, ext, env.Alice.Coldkey.Keypair, uint32(env.Alice.Coldkey.AccInfo.Nonce))
+
+		storageKey, _ := types.CreateStorageKey(env.Client.Meta, "SubtensorModule", "Rho", typetools.Uint16ToBytes(uint16(0)))
+		var newRho types.U16
+		env.Client.Api.RPC.State.GetStorageLatest(storageKey, &newRho)
+
+		require.Equal(t, rho, newRho, "Rho was not updated correctly")
+
+	})
+
+	t.Run("SudoSetActivityCutoff", func(t *testing.T) {
+		t.Parallel()
+		env := setup(t)
+
+		activityCutoff := types.NewU16(1000)
+		sudoCall, _ := SudoSetActivityCutoffCall(env.Client, 0, activityCutoff)
+		ext, _ := NewSudoExt(env.Client, &sudoCall)
+		testutils.SignAndSubmit(t, env.Client, ext, env.Alice.Coldkey.Keypair, uint32(env.Alice.Coldkey.AccInfo.Nonce))
+
+		storageKey, _ := types.CreateStorageKey(env.Client.Meta, "SubtensorModule", "ActivityCutoff", typetools.Uint16ToBytes(uint16(0)))
+		var newActivityCutoff types.U16
+		env.Client.Api.RPC.State.GetStorageLatest(storageKey, &newActivityCutoff)
+
+		require.Equal(t, activityCutoff, newActivityCutoff, "Activity cutoff was not updated correctly")
+	})
+
+	t.Run("SudoSetNetworkRegistrationAllowed", func(t *testing.T) {
+		t.Parallel()
+		env := setup(t)
+
+		registrationAllowed := true
+		sudoCall, _ := SudoSetNetworkRegistrationAllowedCall(env.Client, 0, registrationAllowed)
+		ext, _ := NewSudoExt(env.Client, &sudoCall)
+		testutils.SignAndSubmit(t, env.Client, ext, env.Alice.Coldkey.Keypair, uint32(env.Alice.Coldkey.AccInfo.Nonce))
+
+		storageKey, _ := types.CreateStorageKey(env.Client.Meta, "SubtensorModule", "NetworkRegistrationAllowed", typetools.Uint16ToBytes(uint16(0)))
+		var newNetworkRegistrationAllowed bool
+		env.Client.Api.RPC.State.GetStorageLatest(storageKey, &newNetworkRegistrationAllowed)
+
+		require.Equal(t, registrationAllowed, newNetworkRegistrationAllowed, "Network registration allowed was not updated correctly")
+	})
+
+	t.Run("SudoSetNetworkPowRegistrationAllowed", func(t *testing.T) {
+		t.Parallel()
+		env := setup(t)
+
+		registrationAllowed := true
+		sudoCall, _ := SudoSetNetworkPowRegistrationAllowedCall(env.Client, 0, registrationAllowed)
+		ext, _ := NewSudoExt(env.Client, &sudoCall)
+		testutils.SignAndSubmit(t, env.Client, ext, env.Alice.Coldkey.Keypair, uint32(env.Alice.Coldkey.AccInfo.Nonce))
+
+		storageKey, _ := types.CreateStorageKey(env.Client.Meta, "SubtensorModule", "NetworkPowRegistrationAllowed", typetools.Uint16ToBytes(uint16(0)))
+		var newNetworkPowRegistrationAllowed bool
+		env.Client.Api.RPC.State.GetStorageLatest(storageKey, &newNetworkPowRegistrationAllowed)
+
+		require.Equal(t, registrationAllowed, newNetworkPowRegistrationAllowed, "Network pow registration allowed was not updated correctly")
+	})
+
+	t.Run("SudoSetTargetRegistrationsPerInterval", func(t *testing.T) {
+		t.Parallel()
+		env := setup(t)
+
+		targetRegistrationsPerInterval := types.NewU16(1000)
+		sudoCall, _ := SudoSetTargetRegistrationsPerIntervalCall(env.Client, 0, targetRegistrationsPerInterval)
+		ext, _ := NewSudoExt(env.Client, &sudoCall)
+		testutils.SignAndSubmit(t, env.Client, ext, env.Alice.Coldkey.Keypair, uint32(env.Alice.Coldkey.AccInfo.Nonce))
+
+		storageKey, _ := types.CreateStorageKey(env.Client.Meta, "SubtensorModule", "TargetRegistrationsPerInterval", typetools.Uint16ToBytes(uint16(0)))
+		var newTargetRegistrationsPerInterval types.U16
+		env.Client.Api.RPC.State.GetStorageLatest(storageKey, &newTargetRegistrationsPerInterval)
+
+		require.Equal(t, targetRegistrationsPerInterval, newTargetRegistrationsPerInterval, "Target registrations per interval was not updated correctly")
+	})
+
+	t.Run("SudoSetMinBurn", func(t *testing.T) {
+		t.Parallel()
+		env := setup(t)
+
+		minBurn := types.NewU64(1000)
+		sudoCall, _ := SudoSetMinBurnCall(env.Client, 0, minBurn)
+		ext, _ := NewSudoExt(env.Client, &sudoCall)
+		testutils.SignAndSubmit(t, env.Client, ext, env.Alice.Coldkey.Keypair, uint32(env.Alice.Coldkey.AccInfo.Nonce))
+
+		storageKey, _ := types.CreateStorageKey(env.Client.Meta, "SubtensorModule", "MinBurn", typetools.Uint16ToBytes(uint16(0)))
+		var newMinBurn types.U64
+		env.Client.Api.RPC.State.GetStorageLatest(storageKey, &newMinBurn)
+
+		require.Equal(t, minBurn, newMinBurn, "Min burn was not updated correctly")
+	})
+
+	t.Run("SudoSetMaxBurn", func(t *testing.T) {
+		t.Parallel()
+		env := setup(t)
+
+		maxBurn := types.NewU64(1000)
+		sudoCall, _ := SudoSetMaxBurnCall(env.Client, 0, maxBurn)
+		ext, _ := NewSudoExt(env.Client, &sudoCall)
+		testutils.SignAndSubmit(t, env.Client, ext, env.Alice.Coldkey.Keypair, uint32(env.Alice.Coldkey.AccInfo.Nonce))
+
+		storageKey, _ := types.CreateStorageKey(env.Client.Meta, "SubtensorModule", "MaxBurn", typetools.Uint16ToBytes(uint16(0)))
+		var newMaxBurn types.U64
+		env.Client.Api.RPC.State.GetStorageLatest(storageKey, &newMaxBurn)
+
+		require.Equal(t, maxBurn, newMaxBurn, "Max burn was not updated correctly")
+	})
+
+	t.Run("SudoSetDifficulty", func(t *testing.T) {
+		t.Parallel()
+		env := setup(t)
+
+		difficulty := types.NewU64(1000)
+		sudoCall, _ := SudoSetDifficultyCall(env.Client, 0, difficulty)
+		ext, _ := NewSudoExt(env.Client, &sudoCall)
+		testutils.SignAndSubmit(t, env.Client, ext, env.Alice.Coldkey.Keypair, uint32(env.Alice.Coldkey.AccInfo.Nonce))
+
+		storageKey, _ := types.CreateStorageKey(env.Client.Meta, "SubtensorModule", "Difficulty", typetools.Uint16ToBytes(uint16(0)))
+		var newDifficulty types.U64
+		env.Client.Api.RPC.State.GetStorageLatest(storageKey, &newDifficulty)
+
+		require.Equal(t, difficulty, newDifficulty, "Difficulty was not updated correctly")
+	})
+
+	t.Run("SudoSetMaxAllowedValidators", func(t *testing.T) {
+		t.Parallel()
+		env := setup(t)
+
+		//Rust code has check that max allowed validators is less than or equal to max allowed uids
+		//So we increase max allowed uids first
+		maxAllowedUids := types.NewU16(1000)
+		sudoCall, _ := SudoSetMaxAllowedUidsCall(env.Client, 0, maxAllowedUids)
+		ext, _ := NewSudoExt(env.Client, &sudoCall)
+		testutils.SignAndSubmit(t, env.Client, ext, env.Alice.Coldkey.Keypair, uint32(env.Alice.Coldkey.AccInfo.Nonce))
+		updateUserInfo(t, &env.Alice, env, false) //updated so that nonce is updated for next transaction
+
+		maxAllowedValidators := types.NewU16(10)
+		sudoCall, _ = SudoSetMaxAllowedValidatorsCall(env.Client, 0, maxAllowedValidators)
+		ext, _ = NewSudoExt(env.Client, &sudoCall)
+		testutils.SignAndSubmit(t, env.Client, ext, env.Alice.Coldkey.Keypair, uint32(env.Alice.Coldkey.AccInfo.Nonce))
+
+		storageKey, _ := types.CreateStorageKey(env.Client.Meta, "SubtensorModule", "MaxAllowedValidators", typetools.Uint16ToBytes(uint16(0)))
+		var newMaxAllowedValidators types.U16
+		env.Client.Api.RPC.State.GetStorageLatest(storageKey, &newMaxAllowedValidators)
+
+		require.Equal(t, maxAllowedValidators, newMaxAllowedValidators, "Max allowed validators was not updated correctly")
+	})
+
+	t.Run("SudoSetBondsMovingAverage", func(t *testing.T) {
+		t.Parallel()
+		env := setup(t)
+
+		bondsMovingAverage := types.NewU64(1000)
+		sudoCall, _ := SudoSetBondsMovingAverageCall(env.Client, 0, bondsMovingAverage)
+		ext, _ := NewSudoExt(env.Client, &sudoCall)
+		testutils.SignAndSubmit(t, env.Client, ext, env.Alice.Coldkey.Keypair, uint32(env.Alice.Coldkey.AccInfo.Nonce))
+
+		storageKey, _ := types.CreateStorageKey(env.Client.Meta, "SubtensorModule", "BondsMovingAverage", typetools.Uint16ToBytes(uint16(0)))
+		var newBondsMovingAverage types.U64
+		env.Client.Api.RPC.State.GetStorageLatest(storageKey, &newBondsMovingAverage)
+
+		require.Equal(t, bondsMovingAverage, newBondsMovingAverage, "Bonds moving average was not updated correctly")
+	})
+
+	t.Run("SudoSetBondsPenalty", func(t *testing.T) {
+		t.Parallel()
+		env := setup(t)
+
+		bondsPenalty := types.NewU16(1000)
+		sudoCall, _ := SudoSetBondsPenaltyCall(env.Client, 0, bondsPenalty)
+		ext, _ := NewSudoExt(env.Client, &sudoCall)
+		testutils.SignAndSubmit(t, env.Client, ext, env.Alice.Coldkey.Keypair, uint32(env.Alice.Coldkey.AccInfo.Nonce))
+
+		storageKey, _ := types.CreateStorageKey(env.Client.Meta, "SubtensorModule", "BondsPenalty", typetools.Uint16ToBytes(uint16(0)))
+		var newBondsPenalty types.U16
+		env.Client.Api.RPC.State.GetStorageLatest(storageKey, &newBondsPenalty)
+
+		require.Equal(t, bondsPenalty, newBondsPenalty, "Bonds penalty was not updated correctly")
+	})
+
+	t.Run("SudoSetMaxRegistrationsPerBlock", func(t *testing.T) {
+		t.Parallel()
+		env := setup(t)
+
+		maxRegistrationsPerBlock := types.NewU16(1000)
+		sudoCall, _ := SudoSetMaxRegistrationsPerBlockCall(env.Client, 0, maxRegistrationsPerBlock)
+		ext, _ := NewSudoExt(env.Client, &sudoCall)
+		testutils.SignAndSubmit(t, env.Client, ext, env.Alice.Coldkey.Keypair, uint32(env.Alice.Coldkey.AccInfo.Nonce))
+
+		storageKey, _ := types.CreateStorageKey(env.Client.Meta, "SubtensorModule", "MaxRegistrationsPerBlock", typetools.Uint16ToBytes(uint16(0)))
+		var newMaxRegistrationsPerBlock types.U16
+		env.Client.Api.RPC.State.GetStorageLatest(storageKey, &newMaxRegistrationsPerBlock)
+
+		require.Equal(t, maxRegistrationsPerBlock, newMaxRegistrationsPerBlock, "Max registrations per block was not updated correctly")
+	})
+
+	t.Run("SudoSetSubnetOwnerCut", func(t *testing.T) {
+		t.Parallel()
+		env := setup(t)
+
+		subnetOwnerCut := types.NewU16(1000)
+		sudoCall, _ := SudoSetSubnetOwnerCutCall(env.Client, subnetOwnerCut)
+		ext, _ := NewSudoExt(env.Client, &sudoCall)
+		testutils.SignAndSubmit(t, env.Client, ext, env.Alice.Coldkey.Keypair, uint32(env.Alice.Coldkey.AccInfo.Nonce))
+
+		storageKey, _ := types.CreateStorageKey(env.Client.Meta, "SubtensorModule", "SubnetOwnerCut")
+		var newSubnetOwnerCut types.U16
+		env.Client.Api.RPC.State.GetStorageLatest(storageKey, &newSubnetOwnerCut)
+		require.Equal(t, subnetOwnerCut, newSubnetOwnerCut, "Subnet owner cut was not updated correctly")
+
+	})
+
 }
